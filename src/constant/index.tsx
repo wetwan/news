51195f58b1ae833185799a54c111ab1b13c258e7
ridwanullah <wetwan080@gmail.com>
2025-08-07 06:18:31 +0100

    remove constant data

--- src/constant/index.tsx
+++ src/constant/index.tsx
@@ -1,41 +1,3 @@
-import { images } from "@/assets";
-import type { CommentType, NewsType } from "@/types/types";
-
-export const News: NewsType[] = [
-  {
-    story:
-      "Cloudinary is a SaaS company providing cloud media management services for websites and apps. The company is headquartered in San Jose, California with offices in Israel, England, Poland, and Singapore.",
-    title: "Cloudinary",
-    by: "wetwan",
-    time: { seconds: 569096, nanoseconds: 45678987654 },
-    category: "technology",
-    id: "1",
-    tag: ["technology", "Cloudinary"],
-    image: images.imgae1,
-  },
-  {
-    story:
-      "Cloudinary is a SaaS company providing cloud media management services for websites and apps. The company is headquartered in San Jose, California with offices in Israel, England, Poland, and Singapore.",
-    title: "ronaldo",
-    by: "wetwan",
-    time: { seconds: 5679876, nanoseconds: 45678987654 },
-    category: "sports",
-    id: "2",
-    tag: ["ronaldo", "nigeria", "club"],
-    image: images.imgae2,
-  },
-  {
-    story:
-      "Cloudinary is a SaaS company providing cloud media management services for websites and apps. The company is headquartered in San Jose, California with offices in Israel, England, Poland, and Singapore.",
-    title: "Cloudinary",
-    by: "wetwan",
-    time: { seconds: 56789876, nanoseconds: 45678987654 },
-    category: "Entertainment",
-    id: "3",
-    tag: ["Entertainment", "Cloudinary"],
-    image: images.imgae4,
-  },
-];
 export const Category = [
   "all",
   "Local",
@@ -51,47 +13,4 @@ export const Category = [
   "Lifestyle",
   "Science",
   "Technology",
-];
-
-export const Comment: CommentType[] = [
-  {
-    name: "me",
-    email: " wetwan",
-    time: { seconds: 569096, nanoseconds: 45678987654 },
-    image: images.imgae5,
-    Comment:
-      "Features stories about movies, music, television, celebrities, and other forms of entertainment.",
-    postId: "1",
-    id: "1",
-  },
-  {
-    name: "me1",
-    email: " wetwan1",
-    time: { seconds: 456987654, nanoseconds: 456654 },
-    image: images.imgae1,
-    Comment:
-      "Deals with topics like fashion, health, food, travel, and relationships.",
-    postId: "1",
-    id: "2",
-  },
-  {
-    name: "me3",
-    email: " wetwan",
-    time: { seconds: 569096, nanoseconds: 45678987654 },
-    image: images.imgae2,
-    Comment:
-      "Features stories about movies, music, television, celebrities, and other forms of entertainment.",
-    postId: "2",
-    id: "4",
-  },
-  {
-    name: "me4",
-    email: " wetwan13",
-    time: { seconds: 45698775294, nanoseconds: 456654 },
-    image: images.imgae1,
-    Comment:
-      "Deals with topics like fashion, health, food, travel, and relationships.",
-    postId: "3",
-    id: "54",
-  },
 ];
